288990e4edb5978d1bd617f18e94395cefecbda1
Sachin Chauhan <sc3759@gmail.com>
2019-10-20 03:58:25 +0530

    changes in server for heroku port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express    =    require('express');
 var app        =    express();
+const PORT = process.env.PORT || 5000
 
 // app.get('/',function(req,res){
 //     res.send('Hello world');
@@ -9,7 +10,4 @@ require('./router/main')(app);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
-var server     =    app.listen(3000,function(){
-    console.log("We have started our server on port 3000");
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
